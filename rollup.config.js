f6108e7940ba84e97db21bc4f87abe03cca325f9
MdQayyumShareef <mdqayyumshareef@gmail.com>
2022-05-08 00:28:44 +0530

    update build config

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import { terser } from "rollup-plugin-terser";
+import babel from "rollup-plugin-babel";
+import external from "rollup-plugin-peer-deps-external";
 
 const packageJson = require("./package.json");
 
@@ -11,17 +13,27 @@ export default [
     input: "src/index.ts",
     output: [
       {
-        file: packageJson.main,
+        file: "dist/index.js",
         format: "cjs",
         sourcemap: true,
       },
       {
-        file: packageJson.module,
+        file: "dist/index.es.js",
         format: "esm",
+        exports: "named",
         sourcemap: true,
       },
     ],
     plugins: [
+      babel({
+        exclude: "node_modules/**",
+        presets: [
+          "@babel/preset-env",
+          "@babel/preset-react",
+          "@babel/preset-typescript",
+        ],
+      }),
+      external(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
@@ -29,8 +41,8 @@ export default [
     ],
   },
   {
-    input: "dist/esm/types/src/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    input: "dist/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
     plugins: [dts()],
   },
 ];
